dc58248012aa03427c992e981ba637484c7d3133
Hanz Christian Angelo G Magbal <hanzhmagbal@gmail.com>
2026-03-02 17:58:51 +0000

    Try fixing this error: `Runtime Error: Element type is invalid: expected

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import Link from "next/navigation"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { 
