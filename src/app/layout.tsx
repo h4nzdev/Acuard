7e0d36744d2418cc7f4b046deeef131b4f749c2c
Cmhanz99 <hanzhmagbal@gmail.com>
2026-03-03 22:13:34 +0800

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Acuard | Smart Academic Integrity LMS',
-  description: 'Advanced academic monitoring and integrity analysis system.',
+  title: "Acuard | Smart Academic Integrity LMS",
+  description: "Advanced academic monitoring and integrity analysis system.",
 };
 
 export default function RootLayout({
@@ -16,9 +16,16 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel='icon' href='/logo.png' />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link rel="icon" href={"./logo.png"} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-body antialiased min-h-screen">
         {children}
